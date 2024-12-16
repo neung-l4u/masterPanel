0734cebde7757a2ac93ea90f57fe7842f7e973f6
markL4U <mark@localforyou.com>
2024-12-16 14:04:40 +0700

    Password all function done

--- assets/php/actionPassword.php
+++ assets/php/actionPassword.php
@@ -5,17 +5,23 @@ include '../../assets/db/db.php';
 include "../../assets/db/initDB.php";
 $myID = $_SESSION['id'];
 
-$salt = "L4U";
 $params["action"] = !empty($_REQUEST['act']) ? $_REQUEST['act'] : "";
 $params["id"] = !empty($_REQUEST['id']) ? $_REQUEST['id'] : "";
 $params["editID"] = !empty($_REQUEST['editID']) ? $_REQUEST['editID'] : "";
 $params["status"] = !empty($_REQUEST['status']) ? 1 : 0;
 $params["formAction"] = !empty($_REQUEST['formAction']) ? $_REQUEST['formAction'] : 'add';
 
-if ($params ["action"] == "setStatus"){
-    $update = $db->query('UPDATE `staffs` SET `sStatus` = ? WHERE `staffs`.`sID` = ?;', $params ["status"], $params ["id"]);
-    //echo $update->affectedRows();
-    $params["affected"] = $update->affectedRows();
+if ($params ["action"] == "loadUpdate"){
+    $row = $db->query('SELECT * FROM `passwordmanager` WHERE id = ?;',$params ["id"])->fetchArray();
+    $params["inputType"] = $row["pwType"];
+    $params["inputTeam"] = $row["pwTeam"];
+    $params["inputTeam"] = $row["pwTeam"];
+    $params["inputLevel"] = $row["pwLevel"];
+    $params["inputpwName"] = $row["pwName"];
+    $params["inputAccessLink"] = $row["pwLink"];
+    $params["inputUserName"] = $row["pwUser"];
+    $params["inputPassword"] = $row["pwPass"];
+    $params["inputNote"] = $row["pwNote"];
 
 }elseif ($params ["action"] == "save"){
     $params["txt"] = "Got it";
@@ -26,33 +32,60 @@ if ($params ["action"] == "setStatus"){
         $passwordHash = md5($salt . "Localeats#2023");
     }
 
-    $params["inputName"] = !empty($_REQUEST['inputName']) ? $_REQUEST['inputName'] : "invalid Name";
-    $params["inputEmail"] = !empty($_REQUEST['inputEmail']) ? $_REQUEST['inputEmail'] : "invalid Email";
-    $params["inputPhone"] = !empty($_REQUEST['inputPhone']) ? $_REQUEST['inputPhone'] : "invalid Phone";
-    $params["inputPassword"] = $passwordHash;
-    $params["inputLevel"] = !empty($_REQUEST['inputLevel']) ? $_REQUEST['inputLevel'] : "3";
-    $params["inputStatus"] = !empty($_REQUEST['inputStatus']) ? $_REQUEST['inputStatus'] : "0";
+    $params["inputType"] = !empty($_REQUEST['inputType']) ? $_REQUEST['inputType'] : "invalid Type";
+    $params["inputTeam"] = !empty($_REQUEST['inputTeam']) ? $_REQUEST['inputTeam'] : "invalid Team";
+    $params["inputLevel"] = !empty($_REQUEST['inputLevel']) ? $_REQUEST['inputLevel'] : "invalid Level";
+    $params["inputpwName"] = !empty($_REQUEST['inputpwName']) ? $_REQUEST['inputpwName'] : "invalid Name";
+    $params["inputAccessLink"] = !empty($_REQUEST['inputAccessLink']) ? $_REQUEST['inputAccessLink'] : "invalid Link";
+    $params["inputUserName"] = !empty($_REQUEST['inputUserName']) ? $_REQUEST['inputUserName'] : "invalid Username";
+    $params["inputPassword"] = !empty($_REQUEST['inputPassword']) ? $_REQUEST['inputPassword'] : "invalid Password";
+    $params["inputNote"] = !empty($_REQUEST['inputNote']) ? $_REQUEST['inputNote'] : "";
     $params["by"] = $_SESSION['id'];
 
     if($params ["formAction"]=='add'){
-        $insert = $db->query('INSERT INTO `staffs`
-                                (`sName`, `sEmail`, `sMobile`, `sPassword`, `sStatus`, `sLevel`, `sUpdateBy`) 
-                                VALUES (?, ?, ?, ?, ?, ?, ?);'
-            ,$params["inputName"],$params["inputEmail"],$params["inputPhone"],$params["inputPassword"],$params["inputStatus"],$params["inputLevel"],$params["by"]
+        $insert = $db->query('INSERT INTO `passwordmanager`
+                                (`pwName`, `pwLink`, `pwUser`, `pwPass`, `pwType`, `pwTeam`, `pwLevel`, `pwNote`, `pwCreateBy`) 
+                                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);'
+            ,$params["inputpwName"],
+            $params["inputAccessLink"],
+            $params["inputUserName"],
+            $params["inputPassword"],
+            $params["inputType"],
+            $params["inputTeam"],
+            $params["inputLevel"],
+            $params["inputNote"],
+            $params["by"]
         );
 
         $params["affected"] = $insert->affectedRows();
         $params["insertedID"] = $db->lastInsertID();
+
     }elseif($params ["formAction"]=='edit'){
-        $update = $db->query('UPDATE `staffs` SET 
-                                `sName`= ?, `sEmail` = ?, `sMobile` =?, `sStatus` = ?, `sLevel` = ?, `sUpdateBy`= ?, sUpdateAt = NOW() 
-                                WHERE sID = ? ;'
-            ,$params["inputName"],$params["inputEmail"],$params["inputPhone"],$params["inputStatus"],$params["inputLevel"],$params["by"],$params ["editID"]
+        $update = $db->query('UPDATE `passwordmanager` SET 
+                                `pwName`= ?, `pwLink` = ?, `pwUser` =?, `pwPass` = ?, `pwType` = ?, `pwTeam`= ?, `pwLevel`= ?, `pwNote`= ?, `pwUpdateBy`= ?, pwUpdateAt = NOW() 
+                                WHERE id = ? ;'
+            ,$params["inputpwName"],
+            $params["inputAccessLink"],
+            $params["inputUserName"],
+            $params["inputPassword"],
+            $params["inputType"],
+            $params["inputTeam"],
+            $params["inputLevel"],
+            $params["inputNote"],
+            $params["by"],
+            $params ["editID"]
         );
 
         $params["affected"] = $update->affectedRows();
     }
 
+}elseif ($params ["action"] == "setDelete"){
+    $delete = $db->query('UPDATE `passwordmanager` SET 
+                            `pwType` = 0, `pwDeleteAt` = NOW(), `pwDeleteBy` = ? WHERE id = ? ;'
+        ,$params["by"],
+        $params["id"]
+    );
+    $params["success"] = $delete->affectedRows() > 0;
 }
 
 echo json_encode($params);
