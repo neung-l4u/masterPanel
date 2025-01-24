2cdb7ad69b050aac4acf99144882a98b34369453
BasBYY <bas@localforyou.com>
2025-01-24 15:17:44 +0700

    update number version

--- footer.php
+++ footer.php
@@ -9,6 +9,6 @@
     <strong>Copyright &copy; 2022 <a href="https://www.localforyou.com" target="_blank">Local For You</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 1.2.2 24.01.2025
+        <b>Version</b> 1.2.2 (24.01.2025)
     </div>
 </footer>
