779e947d5860a5d566e74f5ea07a054e87569b1c
neung-l4u <neung@localforyou.com>
2025-01-24 11:21:49 +0700

    update version number

--- footer.php
+++ footer.php
@@ -8,8 +8,7 @@
 <footer class="main-footer">
     <strong>Copyright &copy; 2022 <a href="https://www.localforyou.com" target="_blank">Local For You</a>.</strong>
     All rights reserved.
-    <a href="modules/templates/views/main.php" target="_blank">test</a>
     <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 1.2.0
+        <b>Version</b> 1.2.2
     </div>
 </footer>
