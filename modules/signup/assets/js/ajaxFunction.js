1aa8f6e68ce29ef5d7c567e62438480e42c19d7a
markL4U <mark@localforyou.com>
2025-02-20 08:29:28 +0700

    save link Contract ลง database, เพิ่ม modules generateAgreement, update policy เป็น v1.3.0

--- modules/signup/assets/js/ajaxFunction.js
+++ modules/signup/assets/js/ajaxFunction.js
@@ -890,6 +890,10 @@ const sendMailToL4UTeam = () => {
 }//sendMail
 
 const saveToDB = (stripePayload) => {
+    genLinkPDF();
+    const agreementGenerated = $("#agreementGenerated");
+    let contractURL = agreementGenerated.val();
+
     let cuisineSelected = [];
 
     $("input:checkbox[name='00N2v00000IyVpy']:checked").each(function(){
@@ -1013,7 +1017,8 @@ const saveToDB = (stripePayload) => {
         data: {
             "stripePayload" : stripePayload,
             "payload" : payload,
-            "country" : Country
+            "country" : Country,
+            "contractURL" : contractURL
         }
     });
 
