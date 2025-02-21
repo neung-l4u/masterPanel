0cb66a3f5c944cd9678d756f75e0270734b90293
BasBYY <bas@localforyou.com>
2025-02-21 09:01:03 +0700

    update formemail

--- branch_description.md
+++ branch_description.md
@@ -1,11 +1,18 @@
+250220 bas/feature/formEmail
+>ทำ form สำหรับ ยิง mail ที่ไม่มา
+
+
 250219 mark/hotfix/stateEmail
 >เปลี่ยน section ในอีเมล จาก state เป็น street address
+
 
 250217 mark/feature/logsStripeDB
 >save logs ของ stripe ลง database
 
+
 250215 bas/fix/emailAlert
 >จัดการอีเมลที่ไม่แจ้งเตือนในไฟล์ชื่อ L4UEmailAlert.php
+
 
 241220 fix/layout (bas)
 >- แก้ layout Master Panel ,Gift list ,Convent coin btn to icon box 
---
+++ modules/formemail/views/formEmail.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Email</title>
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container p-3">
+        <div class="row">
+            <div class="col">
+                
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+            <h1>Form Email</h1>
+                <form action="#" method="GET">
+
+                <div class="card ">
+                    <div class="card-body">
+                        <div class="form-group d-flex justify-content-center flex-row">
+                            <label for="formFullName" class="control-label col-form-label">FullName</label>
+                            <div class="col-5">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" id="formFullName" name="formFullName">
+                                </div>
+                            </div>
+
+                            <label for="formDate" class="control-label col-form-label">Form Date</label>
+                            <div class="col-2">
+                                <div class="form-group">
+                                    <input type="date" class="form-control" id="formDate" name="formDate">
+                                </div>
+                            </div>
+
+                            <label for="formCountry" class="control-label col-form-label">Country</label>
+                            <div class="col-2">
+                                <div class="form-group">
+                                    <select id="formCountry" class="form-control">
+                                        <option value="Austialia">Austialia</option>
+                                        <option value="Canada">Canada</option>
+                                        <option value="New Zealand">New Zealand</option>
+                                        <option value="United Kingdom">United Kingdom</option>
+                                        <option value="United States">United States</option>
+                                        <option value="Thailand">Thailand</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>    
+
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="form-group">
+                                            <label for="MainProduct">Main Product</label>
+                                            <input type="text" class="form-control" id="MainProduct" name="MainProduct">
+                                        </div>
+                                    </div>
+                                    <div class="col-2">
+                                        <div class="form-group">
+                                            <label for="formContractPeriod">Contract Period</label>
+                                            <select id="formContractPeriod" class="form-control">
+                                                <option value="No Contract">No Contract</option>
+                                                <option value="3 Months">3 Months</option>
+                                                <option value="6 Months">6 Months</option>
+                                                <option value="12 Months">12 Months</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="form-group">
+                                            <label for="formFirstTimePayment">First Time Payment</label>
+                                            <input type="text" class="form-control" id="formFirstTimePayment" name="formFirstTimePayment">
+                                        </div>
+                                    </div>
+                                    <div class="col-2">
+                                        <div class="form-group">
+                                            <label for="formCoupon">Coupon</label>
+                                            <select id="formCoupon" class="form-control">
+                                                <option value="1TRIAL">1TRIAL</option>
+                                                <option value="FREEWEB">FREEWEB</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-2">
+                                        <div class="form-group">
+                                            <label for="formPaymentMethod">Payment Method</label>
+                                            <select id="formPaymentMethod" class="form-control">
+                                                <option value="Invoice">Invoice</option>
+                                                <option value="Credit Card">Credit Card</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>        
+
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="formRefPerson">RefPerson</label>
+                                <input type="text" class="form-control" id="formRefPerson" name="formRefPerson">
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="formRefPartner">RefPartner</label>
+                                <input type="text" class="form-control" id="formRefPartner" name="formRefPartner">
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="formRefShop">RefShop</label>
+                                <input type="text" class="form-control" id="formRefShop" name="formRefShop">
+                            </div>
+                        </div>
+                    </div>
+                    
+
+                    
+
+                    
+
+                    
+
+                    
+
+                    
+
+                    
+
+                    
+
+                    
+
+                    <div class="form-group">
+                        <label for="formFlyer">Flyer</label>
+                        <input type="text" class="form-control" id="formFlyer" name="formFlyer" value="Do not need">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="formDineIn">DineIn</label>
+                        <input type="text" class="form-control" id="formDineIn" name="formDineIn" value="Do not need">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="formMagnet">Magnet</label>
+                        <input type="text" class="form-control" id="formMagnet" name="formMagnet" value="Do not need">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="formSocialMedia">SocialMedia</label>
+                        <input type="text" class="form-control" id="formSocialMedia" name="formSocialMedia" value="Do not need">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="formMenuDesign">MenuDesign</label>
+                        <input type="text" class="form-control" id="formMenuDesign" name="formMenuDesign" value="Do not need">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="formWebsiteMakeOver">MenuDesign</label>
+                        <input type="text" class="form-control" id="formWebsiteMakeOver" name="formWebsiteMakeOver" value="Do not need">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="formADVPromo">ADVPromo</label>
+                        <input type="text" class="form-control" id="formADVPromo" name="formADVPromo" value="Do not need">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="formWebHosting">formWebHosting</label>
+                        <input type="text" class="form-control" id="formWebHosting" name="formWebHosting" value="Do not need">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="formInfluencer">formInfluencer</label>
+                        <input type="text" class="form-control" id="formInfluencer" name="formInfluencer" value="Do not need">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="formCustomerType">Customer Type</label>
+                        <select id="formCustomerType" class="form-control">
+                            <option value="Thai Massage">Thai Massage</option>
+                            <option value="Credit Card">Thai Restaurant & Take aways</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="formShopName">Shop Name</label>
+                        <input type="text" class="form-control" id="formShopName" name="formShopName">
+                    </div>
+
+                    
+
+                    <div class="form-group">
+                        <label for="formAddress">Address</label>
+                        <input type="text" class="form-control" id="formAddress" name="formAddress">
+                    </div>
+
+                    
+
+                    <div class="form-group">
+                        <label for="formEmail">Email</label>
+                        <input type="text" class="form-control" id="formEmail" name="formEmail">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="formMobile">Mobile</label>
+                        <input type="text" class="form-control" id="formMobile" name="formMobile">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="formBestTime">BestTime</label>
+                        <input type="text" class="form-control" id="formBestTime" name="formBestTime">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="formNote">Note</label>
+                        <textarea class="form-control" id="formNote" name="formNote" row="3"></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="dateStart">dateStart</label>
+                        <input type="date" class="form-control" id="dateStart" name="dateStart">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="formstartprojectNote">Start Project Note</label>
+                        <textarea class="form-control" id="formstartprojectNote" name="formstartprojectNote" row="3"></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="hidden" class="form-control" id="formVersion" name="formVersion" value="2.9.5 UK+Promotion">
+                        <input type="hidden" class="form-control" id="leadSource" name="leadSource" value="Sign Up Form">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary mb-2">Submit</button>
+
+                    
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
