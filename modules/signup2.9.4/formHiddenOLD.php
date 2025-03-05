621d6f488d29b4777a11360ee8fa3f3bd497114a
markL4U <mark@localforyou.com>
2025-03-05 07:13:13 +0700

    just commit backup

---
+++ modules/signup2.9.4/formHiddenOLD.php
@@ -0,0 +1,116 @@
+<div class="hiddenForm none">
+    <input type="hidden" id="signupFormVersion" name="00N9s000000VWbf" value="2.9.7 UK+Promotion" />
+    
+    <input type="hidden" id="firstTimePayment" name="00N2u000000mNZ5">
+    <input type="hidden" id="customerStripeEmail" class="Customer Stripe Email" name="00N9s000000PDD3" value="">
+    <input type="hidden" id="customerStripeID" class="customerStripeID" name="00N9s000000QYtB" value="">
+    <input type="hidden" id="customerStripeIDUSA" class="customerStripeIDUSA" name="00N9s000000VWOl" value="">
+    <input type="hidden" id="myIP">
+    <input type="hidden" id="agent">
+    <input type="hidden" name="00N9s000000QQl5" id="selectedPackages">
+    <input type="hidden" id="usageMainDiscountCode">
+    <input type="hidden" id="usageAddonDiscountCode">
+    <input  id="agreementGenerated" name="00NMq000000UrZx" type="hidden" />
+
+    <select id="currentlyPackage" name="00N9s000000QgXl" class="none" title="Hidden Currently Package">
+        <option value="Pro Online Ordering System">Pro Online Ordering System</option>
+        <option value="Social Media Bundle">Social Media Bundle</option>
+        <option value="Direct Marketing Bundle">Direct Marketing Bundle</option>
+        <option value="Social Media Marketing Solo">Social Media Marketing Solo</option>
+        <option value="Direct Marketing Solo">Direct Marketing Solo</option>
+        <option value="Mega Marketing Solo">Mega Marketing Solo</option>
+        <option value="Website Make Over">Website Make Over</option>
+    </select>
+    <select id="ihdDirectDelivery" name="00N9s000000QgpB" class="none" title="Hidden IHD - Direct Delivery">
+        <option value="">--None--</option>
+        <option value="Requested">Requested</option>
+        <option value="Setting up">Setting up</option>
+        <option value="Live">Live</option>
+        <option value="Cancelled">Cancelled</option>
+    </select>
+    <select id="shopsOwnDriver" name="00N9s000000QgpG" class="none" title="Hidden Shops Own Driver">
+        <option value="">--None--</option>
+        <option value="Requested">Requested</option>
+        <option value="Setting up">Setting up</option>
+        <option value="Live">Live</option>
+        <option value="Cancelled">Cancelled</option>
+    </select>
+    <select id="referredSupplier" name="00N9s000000QhJa" class="none" title="none Referred Supplier">
+        <option value="">--None--</option>
+        <option value="Smile Dinning">Smile Dinning</option>
+        <option value="Wawio">Wawio</option>
+    </select>
+  
+    <input type="hidden" id="paymentRequestTimestamp" maxlength="100" name="00N9s000000QhRe" />
+    <input type="hidden" name="00N9s000000Qlfw" id="initPackage" class="initProject" maxlength="255">
+    <input type="hidden" name="00N9s000000Qlg1" id="initDineInTableOrdering" class="initProject" maxlength="255">
+    <input type="hidden" name="00N9s000000QlgB" id="initAddOnPrintedFlyers" class="initProject" maxlength="255">
+    <input type="hidden" name="00N9s000000QlgG" id="initAddOnFridgeMagnet" class="initProject" maxlength="255">
+    <input type="hidden" name="00N9s000000Qlgf" id="initAddOnAdvPromo" class="initProject" maxlength="255">
+    <input type="hidden" name="00N9s000000QlgV" id="initAddOnSocialMediaPosts" class="initProject" maxlength="255">
+    <input type="hidden" name="00N9s000000Qlga" id="initAddOnInfluencer" class="initProject" maxlength="255">
+    <input type="hidden" name="00N9s000000QlgL" id="initAddOnDineInSystem" class="initProject" maxlength="255">
+    <input type="hidden" name="00N9s000000QlgQ" id="initAddOnDigitalMenuDesign" class="initProject" maxlength="255">
+    <input type="hidden" name="00N9s000000Qlgk" id="initAddOnWebsiteMakeOver" class="initProject" maxlength="255">
+    <input type="hidden" name="00N9s000000Qlgp" id="initAddOnWebHosting" class="initProject" maxlength="255">
+    <input type="hidden" name="00N9s000000QuiM" id="FirstOnlineOrderDiscount" maxlength="50" value="10%" />
+    <select name="00N9s000000Qlgz" id="initIHDDirectDelivery" class="none initProject">
+        <option value="">--None--</option>
+        <option value="Requested">Requested</option>
+        <option value="Setting up">Setting up</option>
+        <option value="Live">Live</option>
+        <option value="Cancelled">Cancelled</option>
+    </select>
+    <select name="00N9s000000Qlgu" id="initShopsOwnDriver" class="none initProject">
+        <option value="">--None--</option>
+        <option value="Requested">Requested</option>
+        <option value="Setting up">Setting up</option>
+        <option value="Live">Live</option>
+        <option value="Cancelled">Cancelled</option>
+    </select>
+    <input type="checkbox" name="00N9s000000Qlg6" id="chkInitPickup" class="none initProject" value="Pickup">
+    <input type="checkbox" name="00N9s000000Qlg6" id="chkInitDineIn" class="none initProject" value="Dine In">
+    <input type="checkbox" name="00N9s000000Qlg6" id="chkInitTableReservations" class="none initProject" value="Table Reservations">
+    <input type="checkbox" name="00N9s000000Qlg6" id="chkInitOwnerDriver" class="none initProject initDriver" value="Delivery by own driver">
+    <input type="checkbox" name="00N9s000000Qlg6" id="chkInitSystemDriver" class="none initProject initDriver" value="Delivery with delivery system">
+    <select name="00N9s000000QnH3" id="initialProductOffering" class="none">
+        <option value="">--None--</option>
+        <option value="Restaurant - Pro Online Ordering System">Restaurant - Pro Online Ordering System</option>
+        <option value="Restaurant - Social Media Marketing Bundle">Restaurant - Social Media Marketing Bundle</option>
+        <option value="Restaurant - Direct Marketing Bundle">Restaurant - Direct Marketing Bundle</option>
+        <option value="Restaurant - Mega Marketing Bundle">Restaurant - Mega Marketing Bundle</option>
+        <option value="Restaurant - Social Media Marketing Solo">Restaurant - Social Media Marketing Solo</option>
+        <option value="Restaurant - Direct Marketing Solo">Restaurant - Direct Marketing Solo</option>
+        <option value="Restaurant - Mega Marketing Solo">Restaurant - Mega Marketing Solo</option>
+        <option value="Massage - Localforyou Booking System">Massage - Localforyou Booking System</option>
+        <option value="Massage - Social Media Marketing Bundle">Massage - Social Media Marketing Bundle</option>
+        <option value="Massage - Direct Marketing Bundle">Massage - Direct Marketing Bundle</option>
+        <option value="Massage - Mega Marketing Bundle">Massage - Mega Marketing Bundle</option>
+        <option value="Massage - Social Media Marketing Solo">Massage - Social Media Marketing Solo</option>
+        <option value="Massage - Direct Marketing Solo">Massage - Direct Marketing Solo</option>
+        <option value="Massage - Mega Marketing Solo">Massage - Mega Marketing Solo</option>
+        <option value="Promotions - Pro + $1 Trial">Promotions - Pro + $1 Trial</option>
+        <option value="Promotions - Social Bundle + $1 Trial">Promotions - Social Bundle + $1 Trial</option>
+        <option value="Promotions - Direct Bundle + $1 Trial">Promotions - Direct Bundle + $1 Trial</option>
+        <option value="Promotions - Mega Bundle + $1 Trail">Promotions - Mega Bundle + $1 Trail</option>
+        <option value="Promotions - Social + $1 Smile">Promotions - Social + $1 Smile</option>
+        <option value="Promotions - Direct + $1 Smile">Promotions - Direct + $1 Smile</option>
+        <option value="Promotions - Mega + $1 Trail">Promotions - Mega + $1 Trail</option>
+        <option value="Promotions - Booking System + Freeweb">Promotions - Booking System + Freeweb</option>
+        <option value="Promotions - Social Bundle + Freeweb">Promotions - Social Bundle + Freeweb</option>
+        <option value="Promotions - Direct Bundle + Freeweb">Promotions - Direct Bundle + Freeweb</option>
+        <option value="Promotions - Mega Bundle + Freeweb">Promotions - Mega Bundle + Freeweb</option>
+        <option value="Promotions - Social + Freeweb">Promotions - Social + Freeweb</option>
+        <option value="Promotions - Direct + Freeweb">Promotions - Direct + Freeweb</option>
+        <option value="Promotions - Mega + Freeweb">Promotions - Mega + Freeweb</option>
+        <option value="In House Delivery (IHD) Marketplace Setup">In House Delivery (IHD) Marketplace Setup</option>
+        <option value="Website Makeover (Single Purchase)">Website Makeover (Single Purchase)</option>
+    </select>
+    <input  id="ContractStartDate" name="00NMq000000Siu9" size="12" type="text" value="" />
+    <select  id="ContractPeriod" name="00NMq000000Sinh">
+        <option value="">--None--</option>
+        <option value="No contract" selected>No contract</option>
+        <option value="3 months">3 months</option>
+        <option value="12 months">12 months</option>
+    </select>
+</div>
