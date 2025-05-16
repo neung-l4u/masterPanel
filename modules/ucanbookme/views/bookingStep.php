2a89d481041c8581194a4b62013047475cbfe807
neung-l4u <neung@localforyou.com>
2025-05-16 09:19:19 +0700

    Generate .ics file

--- modules/ucanbookme/views/bookingStep.php
+++ modules/ucanbookme/views/bookingStep.php
@@ -24,6 +24,19 @@ $tomorrow = date("Y-m-d", strtotime("+1 day"));
         .step-section.active {
             display: block;
         }
+        #country{
+            width:400px;
+        }
+        #sales{
+            width:500px;
+        }
+        #time{
+            width:350px;
+        }
+        ::placeholder {
+            color: #cccccc !important;
+            opacity: 1; /* Firefox */
+        }
     </style>
 </head>
 <body>
@@ -57,7 +70,7 @@ $tomorrow = date("Y-m-d", strtotime("+1 day"));
                             <select id="shop_type" name="shop_type" class="form-control form-select mb-3" >
                                 <option></option>
                                 <?php
-                                $shopType = $db->query('SELECT * FROM `tb_shopType`')->fetchAll();
+                                $shopType = $db->query('SELECT * FROM `tb_shopType` WHERE status = ?',1)->fetchAll();
                                 foreach ($shopType as $row) {
                                 ?>
                                 <option value="<?php echo $row['id']; ?>"><?php echo $row['name']; ?></option>
@@ -134,17 +147,23 @@ $tomorrow = date("Y-m-d", strtotime("+1 day"));
             <div class="step-section" id="step-6">
                 <label>6. ข้อมูลลูกค้า</label>
                 <div class="d-flex flex-column gap-3 mb-3">
-                    <input type="text" name="shop_name" class="form-control mb-2" placeholder="ชื่อร้าน" >
-                    <input type="text" name="customer_name" class="form-control mb-2" placeholder="ชื่อเจ้าของร้าน"
-                           >
-                    <input type="email" name="contact_email" class="form-control mb-2" placeholder="อีเมล">
-                    <input type="text" name="contact_phone" class="form-control mb-2" placeholder="เบอร์โทร">
-                    <input type="text" name="line_id" class="form-control mb-2" placeholder="LINE ID">
-                    <input type="text" name="whatsapp" class="form-control mb-2" placeholder="WhatsApp">
+                    <label for="shop_name">Shop name</label>
+                        <input type="text" id="shop_name" name="shop_name" class="form-control mb-2" autocomplete="off" placeholder="ชื่อร้าน" >
+                    <label for="customer_name">Customer name</label>
+                        <input type="text" id="customer_name" name="customer_name" class="form-control mb-2" autocomplete="off" placeholder="ชื่อเจ้าของร้าน">
+                    <label for="contact_email">Contact email</label>
+                        <input type="email" id="contact_email" name="contact_email" class="form-control mb-2" autocomplete="off" placeholder="อีเมล">
+                    <label for="contact_phone">Contact phone</label>
+                        <input type="text" id="contact_phone" name="contact_phone" class="form-control mb-2" autocomplete="off" placeholder="เบอร์โทร">
+                    <label for="line_id">Line ID</label>
+                        <input type="text" id="line_id" name="line_id" class="form-control mb-2" autocomplete="off" placeholder="LINE ID">
+                    <label for="whatsapp">WhatsApp</label>
+                        <input type="text" id="whatsapp" name="whatsapp" class="form-control mb-2" autocomplete="off" placeholder="WhatsApp">
                 </div>
                 <div class="d-flex flex-row gap-3 mb-3">
                     <button type="button" class="btn btn-secondary" onclick="prevStep(5)">Previous</button>
                     <button type="submit" class="btn btn-success">Book now!!</button>
+                    <a href="#" onclick="sendEmail();">Alert</a>
                 </div>
             </div>
         </form>
@@ -157,7 +176,21 @@ $tomorrow = date("Y-m-d", strtotime("+1 day"));
 <script src="../assets/libs/flatpickr/flatpickr.js"></script>
 
 <script>
+    const shop_type = $('#shop_type');
     const country = $('#country');
+    const city = $('#city');
+    const sales = $('#sales');
+    const date = $('#date');
+    const time = $('#time');
+    const shop_name = $('#shop_name');
+    const customer_name = $('#customer_name');
+    const contact_email = $('#contact_email');
+    const contact_phone = $('#contact_phone');
+    const line_id = $('#line_id');
+    const whatsapp = $('#whatsapp');
+
+    let appointmentDetail = {};
+    let sendEmailPayload = {};
 
     $(document).ready(function () {
         $('#shop_type').select2({placeholder: 'เลือกประเภทร้าน',theme: 'bootstrap-5'});
@@ -180,7 +213,7 @@ $tomorrow = date("Y-m-d", strtotime("+1 day"));
                 if (res.status === 'ok') {
                     let newOptions = res.data.map(user => new Option(user.text, user.id, false, false));
                     $('#sales').append(newOptions).trigger('change');
-                    $("#country").val($("#country option:first").val());
+                    //$("#country").val($("#country option:first").val());
                 }
             });
         });//country change
@@ -205,12 +238,16 @@ $tomorrow = date("Y-m-d", strtotime("+1 day"));
             e.preventDefault();
             $.post('../models/save_appointment.php', $(this).serialize(), function (res) {
                 if (res.status === 'ok') {
+                    sendEmail();
+                    bookCalendar();
                     alert('✅ จองนัดเรียบร้อย');
-                    location.href = 'booking_success.php';
+
+                    //location.href = 'booking_success.php';
                 } else {
                     alert('❌ ' + res.message);
                 }
             });
+
         });//form submit
 
     });//ready
@@ -223,6 +260,162 @@ $tomorrow = date("Y-m-d", strtotime("+1 day"));
     function prevStep(step) {
         $('.step-section').removeClass('active');
         $('#step-' + step).addClass('active');
+    }
+
+    function sendEmail() {
+        sendEmailPayload = {
+            "staff_id": sales.val(),
+            "staff_email": getStaffEmail(sales.val()),
+            "staff_name": $("#sales option:selected").text(),
+            "staff_nickname": getNickname($("#sales option:selected").text()),
+            "created_by": "1",
+            "shop_type_id": shop_type.val(),
+            "shop_type": $("#shop_type option:selected").text(),
+            "country": country.val(),
+            "city": city.val(),
+            "date": date.val(),
+            "time": time.val(),
+            "customer_name": customer_name.val(),
+            "shop_name": shop_name.val(),
+            "contact_email": contact_email.val(),
+            "contact_phone": contact_phone.val(),
+            "line_id": line_id.val(),
+            "whatsapp": whatsapp.val(),
+            "formVersion": "1.0.0"
+        };
+
+        console.log("we call webhook UCanBookMe - email alert");
+        console.log(sendEmailPayload);
+
+         const sendEmail = $.ajax({
+             type: "POST",
+             crossDomain: true,
+             dataType: 'json',
+             url: "https://hook.us1.make.com/yk8yef5sm9m5y4gr8qfj71ynmsorhz7d",
+             data: sendEmailPayload
+         });
+
+         sendEmail.done(function (res) {
+             console.log("send Email done");
+             console.log("return = ",res);
+         });
+
+         sendEmail.fail(function(xhr, status, error) {
+             console.log("ajax webhook fail!!");
+             console.log(status + ': ' + error);
+             alert("Send fail!!");
+         });
+    }//sendEmail
+
+    function bookCalendar() {
+        appointmentDetail = {
+            "staff_email": getStaffEmail(sales.val()),
+            "staff_name": $("#sales option:selected").text(),
+            "staff_nickname": getNickname($("#sales option:selected").text()),
+            "shop_type": $("#shop_type option:selected").text(),
+            "country": country.val(),
+            "city": city.val(),
+            "startDate": date.val(),
+            "startTime": time.val(),
+            "endDate": date.val(),
+            "endTime": addMinutes(time.val(),15),
+            "dtStamp": toUTCFormat(date.val(), time.val(),0),
+            "dtStart": toUTCFormat(date.val(), time.val(),0),
+            "dtEnd": toUTCFormat(date.val(), addMinutes(time.val(),15),0),
+            "customer_name": customer_name.val(),
+            "shop_name": shop_name.val(),
+            "contact_email": contact_email.val(),
+            "contact_phone": contact_phone.val(),
+            "line_id": line_id.val(),
+            "whatsapp": whatsapp.val(),
+            "formVersion": "1.0.0"
+        };
+
+        console.log("we call webhook UCanBookMe - Appointment created");
+        const makeAppointment = $.ajax({
+            type: "POST",
+            crossDomain: true,
+            dataType: 'json',
+            url: "https://hook.us1.make.com/hg8rqnmfuxry86lq4ylr967j9uifhlt1",
+            data: appointmentDetail
+        });
+
+        makeAppointment.done(function (res) {
+            console.log("make Appointment done");
+            console.log("return = ",res);
+        });
+
+        makeAppointment.fail(function(xhr, status, error) {
+            console.log("make Appointment webhook fail!!");
+            console.log(status + ': ' + error);
+            alert("Send fail!!");
+        });
+    }//bookCalendar
+
+
+    function getStaffEmail(id) {
+        /*const map = {
+            17: 'boom@localforyou.com',
+            18: 'dear@localforyou.com',
+            24: 'honey@localforyou.com',
+            35: 'pluem@localforyou.com',
+            38: 'pruek@localforyou.com',
+            47: 'toffee@localforyou.com',
+            62: 'ball@localforyou.com',
+            72: 'lani@localforyou.com',
+            76: 'naya@localforyou.com',
+            79: 'gun@localforyou.com',
+            84: 'aon@localforyou.com',
+        };*/
+
+        const map = {
+            17: 'neung@localforyou.com',
+            18: 'neung@localforyou.com',
+            24: 'neung@localforyou.com',
+            35: 'neung@localforyou.com',
+            38: 'neung@localforyou.com',
+            47: 'neung@localforyou.com',
+            62: 'neung@localforyou.com',
+            72: 'neung@localforyou.com',
+            76: 'neung@localforyou.com',
+            79: 'neung@localforyou.com',
+            84: 'neung@localforyou.com',
+        };
+        return map[id] || 'administrator@localforyou.com';
+    }
+
+    function getNickname(fullName) {
+        if (!fullName) return '';
+        return fullName.trim().split(' ')[0];
+    }
+
+    function toUTCFormat(dateStr, timeStr, offsetHours = 0) {
+        const [year, month, day] = dateStr.split('-').map(Number);
+        const [hour, minute, second] = timeStr.split(':').map(Number);
+
+        const utcMillis = Date.UTC(year, month - 1, day, hour - offsetHours, minute, second || 0);
+        const utcDate = new Date(utcMillis);
+
+        const y = utcDate.getUTCFullYear();
+        const m = String(utcDate.getUTCMonth() + 1).padStart(2, '0');
+        const d = String(utcDate.getUTCDate()).padStart(2, '0');
+        const h = String(utcDate.getUTCHours()).padStart(2, '0');
+        const min = String(utcDate.getUTCMinutes()).padStart(2, '0');
+        const s = String(utcDate.getUTCSeconds()).padStart(2, '0');
+
+        return `${y}${m}${d}T${h}${min}${s}Z`;
+    }
+
+    function addMinutes(timeStr, minutesToAdd) {
+        const [hour, minute, second] = timeStr.split(':').map(Number);
+        const date = new Date(0, 0, 0, hour, minute, second || 0);
+        date.setMinutes(date.getMinutes() + minutesToAdd);
+
+        const h = String(date.getHours()).padStart(2, '0');
+        const m = String(date.getMinutes()).padStart(2, '0');
+        const s = String(date.getSeconds()).padStart(2, '0');
+
+        return `${h}:${m}:${s}`;
     }
 </script>
 </body>
