7021c197dde3fbf33a18e4a900eb70ecd6600f1b
BasBYY <bas@localforyou.com>
2025-05-21 14:43:22 +0700

    add icon new tab to dreamscape and play icon to youtube L4U update btn submit to monday

--- modules/signup/form_header.php
+++ modules/signup/form_header.php
@@ -9,6 +9,6 @@
 
 <link rel="stylesheet" href="assets/css/bootstrap.5.2.3.min.css">
 <link rel='stylesheet' href='assets/css/FontAwesomeFree.5.15.2.all.min.css'>
-<link rel="stylesheet" href="assets/css/defaultForm.css">
+<link rel="stylesheet" href="assets/css/defaultForm.css?v=2.0.0">
 <link rel="stylesheet" href="assets/css/tooltips.css">
-<script src="assets/js/FontAwesomeKitConfig.js"></script>
+<script src="assets/js/FontAwesomeKitConfig.js?v=2.0.0"></script>
