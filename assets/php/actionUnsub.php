f1744eb5ba89838333e433db785944d6d21a8e1a
BasBYY <bas@localforyou.com>
2025-05-29 09:43:19 +0700

    update unsub and add ?v=1.0.0 templatesubmittion

--- assets/php/actionUnsub.php
+++ assets/php/actionUnsub.php
@@ -5,10 +5,16 @@ include '../../assets/db/db.php';
 include "../../assets/db/initDB.php";
 
 
-$params["action"] = !empty($_GET['act']) ? $_GET['act'] : "No Action";
-$params["id"] = !empty($_GET['id']) ? $_GET['id'] : "9999";
+$params["action"] = !empty($_POST['act']) ? $_POST['act'] : "No Action";
+$params["id"] = !empty($_POST['id']) ? $_POST['id'] : "9999";
 
  $row = $db->query('SELECT * FROM `Cancellation` WHERE id = ?;',$params ["id"])->fetchArray();
+ $shopname = !empty($row["shopname"]) ? $row["shopname"] : "-";
+ $feedback = !empty($row["feedback"]) ? $row["feedback"] : "-";
+ $fullname = !empty($row["firstname"]." ".$row["lastname"]) ? $row["firstname"]." ".$row["lastname"] : "-";
+ $email = !empty($row["email"]) ? $row["email"] : "-";
+ $mobile = !empty($row["mobile"]) ? $row["mobile"] : "-";
+ $address = !empty($row["address"]." ,".($row["city"])." ,".($row["state"])." ,".($row["zip"])." ,".($row["county"])) ? $row["address"]." ,".($row["city"])." ,".($row["state"])." ,".($row["zip"])." ,".($row["county"]) : "-";
 
 ?>
 
@@ -16,27 +22,27 @@ $params["id"] = !empty($_GET['id']) ? $_GET['id'] : "9999";
         <tbody>
             <tr>
                 <th scope="row">Shop Name</th>
-                <td><?php echo $row["shopname"];?></td>
+                <td><?php echo $shopname;?></td>
             </tr>
             <tr>
                 <th scope="row">Address</th>
-                <td><?php echo $row["address"].", ".$row["city"].", ".$row["state"].", ".$row["zip"].", ".$row["county"];?></td>
+                <td><?php echo $address;?></td>
             </tr>
             <tr>
                 <th scope="row">Name</th>
-                <td><?php echo $row["firstname"]." ".$row["lastdate"];?></td>
+                <td><?php echo $fullname;?></td>
             </tr>
             <tr>
                 <th scope="row">Email</th>
-                <td><?php echo $row["email"];?></td>
+                <td><?php echo $email;?></td>
             </tr>
             <tr>
                 <th scope="row">Mobile</th>
-                <td><?php echo $row["mobile"];?></td>
+                <td><?php echo $mobile;?></td>
             </tr>
             <tr>
                 <th scope="row">FeedBack</th>
-                <td><?php echo $row["feedback"];?></td>
+                <td><?php echo $feedback;?></td>
             </tr>
         </tbody>
     </table>
