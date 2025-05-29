6f7f2fcb13e1f42a41499d414c87bbcb2f0e09b6
neung-l4u <neung@localforyou.com>
2025-05-30 06:28:34 +0700

    add libs

---
+++ modules/checkin/assets/php/share_function.php
@@ -0,0 +1,18 @@
+<?php
+/*
+----- function getRandomPic ------
+parameters
+    $shopType = restaurant, massage
+    $picType = folder name in assets/img/sample_image/shopType/ e.g., bg, logo, other, review, service, food
+    $fullPath = true, false (default false)
+
+  example 1:
+    $getPic = getRandomPic("restaurant", "logo");
+    will return >> bg5.webp
+
+  example 2:
+    $getPic = getRandomPic("massage", "bg", true);
+    will return >> ../assets/img/sample_image/massages/bg/bg5.webp
+*/
+
+?>
---
+++ modules/checkin/views/footer.php
@@ -0,0 +1,3 @@
+<footer class="d-flex justify-content-center align-items-center text-center">
+    copyright © 2017 by localforyou.com
+</footer>
