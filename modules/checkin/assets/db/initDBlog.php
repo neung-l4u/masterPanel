a1cfb39651c91929bd1473b28b4db2af37a44f87
BasBYY <bas@localforyou.com>
2025-06-04 11:10:45 +0700

    Add HR Past Time To Database

---
+++ modules/checkin/assets/db/initDBlog.php
@@ -0,0 +1,10 @@
+<?php
+date_default_timezone_set("Asia/Bangkok");
+error_reporting(E_ERROR | E_PARSE);
+
+$dbHost = '85.187.128.54';
+$dbUser = 'localfor_logSystems';
+$dbPass = '-MP1h$r_k?x!';
+$dbName = 'localfor_logSystems';
+
+$db = new db($dbHost, $dbUser, $dbPass, $dbName);
