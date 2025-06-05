a398b5f037758767b742c40acbdd833d4b77fa4f
markL4U <mark@localforyou.com>
2025-06-05 16:10:44 +0700

    done

--- modules/checkin/controllers/timeOff.js
+++ modules/checkin/controllers/timeOff.js
@@ -1,3 +1,36 @@
+function getStaffTeam(id) {
+    const staffID = id;
+    const $staffName = $("#staffName");
+    const $staffTeam = $("#staffTeam");
+    const $manager = $("#manager");
+    const $staffNameVal = $staffName.val();
+
+    $.ajax({
+        url: '../models/timeOff.php',
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            staffID: staffID
+        }
+    }).done((res) => {
+        console.log(res);
+        if (res.team === "Information Technology") {
+            res.team = "IT";
+        }
+        const staffNameArr = res.staffName.split(" ");
+        let staffName = staffNameArr[0];
+        const managerNameArr = res.managerName.split(" ");
+        let managerName = managerNameArr[0];
+
+        $staffName.val($staffNameVal);
+        $staffTeam.val(res.team).attr("selected", true);
+        $manager.val(res.managerNickName + " " + managerName);
+    }).fail(() => {
+        alert("Failed to select team");
+    });
+
+}
+
 const handleFileUpload = (input) => {
     const $staffName = $("#staffName").val().trim();
     const $form = $(input).closest("form");
