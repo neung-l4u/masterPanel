8018cc83e9a6bb53929746fb13335aaf154023eb
markL4U <mark@localforyou.com>
2025-06-12 16:49:06 +0700

    commit

--- api/signup/getReport.php
+++ api/signup/getReport.php
@@ -1,19 +1,17 @@
 <?php
 require_once '../../assets/db/db.php';
 require_once '../../assets/db/initDB.php';
-
-//http://localhost/masterPanel/api/signup/getReport.php?date=1996/01/20
-
 $getDateString = $_GET["date"];
 
 $getDate = new DateTime($getDateString);
 $Date = $getDate->format('Y-m-d');
+
+$dateIndex = $getDate->format('w');
+
+
+
 echo $Date;
-
-$dateIndex = $getDateString->
-
-$startDate = new DateTime();
-$endDate = new DateTime();
-
+echo "<br>";
+echo $dateIndex;
 
 ?>
