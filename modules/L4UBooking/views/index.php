e0cd80f3cd7798f084c14e7261599a49895e7b1c
neung-l4u <neung@localforyou.com>
2025-07-02 07:45:03 +0700

    update version description.

--- modules/L4UBooking/views/index.php
+++ modules/L4UBooking/views/index.php
@@ -49,12 +49,13 @@ $currentPage = basename($_SERVER['PHP_SELF']);
             <a href="bookingStep.php" class="btn btn-primary">Book now</a>
         </div>
         <section style="min-height: 50vh;">
-            <h4><i class="bi bi-card-list"></i> Version 1.1</h4>
+            <h4><i class="bi bi-card-list"></i> Version 1.2</h4>
             <ul>
                 <li>Save booking information to the database</li>
                 <li>Send confirmation email to customers, sales.</li>
                 <li>Send a calendar link via email after booking.</li>
                 <li>Reminders three times.</li>
+                <li>add Select timezone manual</li>
             </ul>
         </section>
     </main>
