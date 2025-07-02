dc1467cb6461a1867751499f3ff78f9d4a77e771
neung-l4u <neung@localforyou.com>
2025-07-02 07:51:18 +0700

    fir warning

--- modules/L4UBooking/views/index.php
+++ modules/L4UBooking/views/index.php
@@ -37,7 +37,7 @@ $currentPage = basename($_SERVER['PHP_SELF']);
                 modal
             </button>
         </div>-->
-        <nav class="mb-4" style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+        <nav class="mb-4" aria-label="breadcrumb">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item active" aria-current="page"><i class="bi bi-house-fill"></i> Home</li>
             </ol>
