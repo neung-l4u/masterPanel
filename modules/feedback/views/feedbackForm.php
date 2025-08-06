f3f24c238a7d383bb690252a869b91fc28a4d920
markL4U <mark@localforyou.com>
2025-08-06 07:29:08 +0700

    fix feedback form

--- modules/feedback/views/feedbackForm.php
+++ modules/feedback/views/feedbackForm.php
@@ -31,26 +31,6 @@ global $db;
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img src="../assets/img/L4U-Site-Icon.png" alt="logo" style="width: 50px;"/></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="index.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="../views/feedbackForm.php">Form</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../views/entries.php">Entries</a>
-                </li>
-            </ul>
-            <span class="navbar-text">
-                logout
-            </span>
-        </div>
     </div>
 </nav>
 
