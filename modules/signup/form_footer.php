ff472588a322dff7a7cd695f5f2fedce55765d91
BasBYY <bas@localforyou.com>
2025-08-14 11:09:46 +0700

    อัพเดท ขอ 3. ของโปรโมชั่น

--- modules/signup/form_footer.php
+++ modules/signup/form_footer.php
@@ -31,9 +31,9 @@
 <script src="assets/js/date_format.js"></script>
 <script src="assets/js/getUserAgent.js"></script>
 <script src="assets/js/util.js?v=2.0.2"></script>
-<script src="assets/js/ajaxFunction.js?v=2.0.8"></script>
+<script src="assets/js/ajaxFunction.js?v=2.0.9"></script>
 <script src="https://api.ipify.org?format=jsonp&callback=getIP"></script>
 <script src="assets/js/popper.2.11.5.min.js"></script>
 <script src="assets/js/bootstrap-multi-step-form.js?v=2.0.2"></script>
 
-<input type="hidden" id="signupFormVersion" name="signupFormVersion" value="2.10.5" />
+<input type="hidden" id="signupFormVersion" name="signupFormVersion" value="2.10.6" />
