61ae8c53462dd7821dfad3aea1beb249c157ad66
neung-l4u <neung@localforyou.com>
2025-09-26 18:47:26 +0700

    recovery files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'almasaeed2010/adminlte',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '48fec039c62b585b1f929d6d4a57bc116c7e02be',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'almasaeed2010/adminlte' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '48fec039c62b585b1f929d6d4a57bc116c7e02be',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
