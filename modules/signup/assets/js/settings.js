628b1842dce5b9ca26be91e1939e98da8f59d6e9
markL4U <mark@localforyou.com>
2025-11-25 10:22:57 +0700

    add product pro online ordering thailand

--- modules/signup/assets/js/settings.js
+++ modules/signup/assets/js/settings.js
@@ -4,7 +4,7 @@ const settings = {
     card: "https://payments.localforyou.com/api/payment",
     invoice: "https://payments.localforyou.com/api/invoice",
   },
-  url_getProductList: "assets/API/B-Price-2511071458-addAI.json",
+  url_getProductList: "assets/API/B-Price-2511071458-addProOnlineOrderingTH.json",
   url_getPrice: "assets/API/price.php",
   url_getStates: "assets/statics/states.json",
   url_authentication: "assets/API/authentication.php",
