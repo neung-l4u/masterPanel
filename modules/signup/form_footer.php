541a5dddcbb37542eb324efb76ce0833d645fd59
BasBYY <bas@localforyou.com>
2025-11-26 06:34:58 +0700

    add price sign up form and fix voucher

--- modules/signup/form_footer.php
+++ modules/signup/form_footer.php
@@ -27,7 +27,7 @@
 <script src="assets/js/jquery.3.6.0.min.js"></script>
 <script src="assets/js/bootstrap5.0.2.bundle.min.js"></script>
 <script src="assets/js/global_data.js?v=2.0.4"></script>
-<script src="assets/js/settings.js?v=2.0.16"></script>
+<script src="assets/js/settings.js?v=2.0.17"></script>
 <script src="assets/js/date_format.js"></script>
 <script src="assets/js/getUserAgent.js"></script>
 <script src="assets/js/util.js?v=2.0.4"></script>
@@ -36,4 +36,4 @@
 <script src="assets/js/popper.2.11.5.min.js"></script>
 <script src="assets/js/bootstrap-multi-step-form.js?v=2.0.14"></script>
 
-<input type="hidden" id="signupFormVersion" name="signupFormVersion" value="2.10.18" />
+<input type="hidden" id="signupFormVersion" name="signupFormVersion" value="2.10.19" />
