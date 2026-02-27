10c15e57dfd4e728933efaaf67d52908c4ade59b
BasBYY <bas@localforyou.com>
2026-02-27 14:35:20 +0700

    update signupMini

---
+++ modules/signupMini/--index.php
@@ -0,0 +1,221 @@
+<?php
+date_default_timezone_set("Asia/Bangkok");
+$currentDate = date('d/m/Y');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign up Form Mini</title>
+    <link rel="stylesheet" href="assets/css/index.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="container">
+        <form class="p-5" action="#" method="POST" id="miniForm">
+
+            <div class="card">
+                <div class="card-header">
+                    <h1>Free Thai Demo</h1>
+                    <small>Enter your details below and we’ll show you how our Online Ordering, Booking & Marketing will work in your Thai Restaurant & Massage and Spa!</small>
+                </div>
+
+
+                <div class="card-body p-4">
+
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label class="font-weight-bold">Sign Up Date : <?php echo $currentDate; ?></label>
+
+                            </div>
+                        </div> <!-- col -->
+                    </div> <!-- row -->
+
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="first_name" class="font-weight-bold">First Name <span class="text-danger">*</span> </label>
+                                <input type="text" id="first_name" class="form-control" maxlength="40" name="first_name" placeholder="First name">
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="last_name" class="font-weight-bold">Last Name <span class="text-danger">*</span> </label>
+                                <input type="text" id="last_name" class="form-control" maxlength="80" name="last_name" placeholder="Last name">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="email" class="font-weight-bold">Email <span class="text-danger">*</span> </label>
+                                <input type="text" id="email" class="form-control" name="email" placeholder="email@website.com">
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="mobile" class="font-weight-bold">Mobile <span class="text-danger">*</span> </label>
+                                <input type="text" id="mobile" class="form-control" name="mobile" placeholder="012345678">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="contactTime" class="font-weight-bold">Best time to Contact</label>
+                                <input id="contactTime" class="form-control" name="contactTime" placeholder="10:00am" type="text">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="company" class="font-weight-bold">Trading name</label>
+                                <input type="text" id="company" class="form-control" name="company">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="shopName" class="font-weight-bold">Shop Name <span class="text-danger">*</span></label>
+                                <input id="shopName" class="form-control" placeholder="your shop name" name="shopName" type="text" />
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="shopType" class="font-weight-bold">Shop Type <span class="text-danger">*</span> </label>
+                                <select id="shopType" class="form-control" name="shopType" title="Customer_Type">
+                                    <option value="Thai Restaurants &amp; Takeaways">Thai Restaurants &amp; Takeaways</option>
+                                    <option value="Thai Massage">Thai Massage</option>
+                                    <option value="Restaurants &amp; Takeaways">Restaurants &amp; Takeaways</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="url" class="font-weight-bold">Website or Social media</label>
+                                <input type="text" id="url" class="form-control" name="url" placeholder="www.localforyou.com">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="city" class="font-weight-bold">City</label>
+                                <input id="city" class="form-control" name="city" placeholder="Queenland" type="text">
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="country" class="font-weight-bold">Country</label>
+                                <select id="country" class="form-control" name="country" onchange="setMoney();">
+                                    <option value="Australia">Australia</option>
+                                    <option value="Canada">Canada</option>
+                                    <option value="New Zealand">New Zealand</option>
+                                    <option value="United Kingdom">United Kingdom</option>
+                                    <option value="United States">United States</option>
+                                    <option value="Thailand">Thailand</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row" style="display:none;">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="currency" class="font-weight-bold">
+                                    Currency
+                                    <span class="text-danger">*</span>
+                                </label>
+                                <select id="currency" class="form-control" name="currency">
+                                    <option value="AUD" selected="selected">AUD - Australian Dollar</option>
+                                    <option value="CAD">CAD - Canadian Dollar</option>
+                                    <option value="NZD">NZD - New Zealand Dollar</option>
+                                    <option value="GBP">GBP - British Pound</option>
+                                    <option value="USD">USD - U.S. Dollar</option>
+                                    <option value="THB">THB - Thai Baht</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="interest" class="font-weight-bold">Interesting in</label>
+                                <select id="interest" class="form-control" name="interest" title="Lead Interesting in">
+                                    <option value="Not specified" selected>-- Not specified --</option>
+                                    <option value="Online Ordering System">Online Ordering System</option>
+                                    <option value="Booking System">Booking System</option>
+                                    <option value="Massage &amp; Spa">Massage & Spa</option>
+                                    <option value="Pro Shopping Cart">Pro Shopping Cart</option>
+                                    <option value="Social Media Marketing">Social Media Marketing</option>
+                                    <option value="Social Media Bundle">Social Media Bundle</option>
+                                    <option value="Other">Other</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="comments" class="font-weight-bold">Comments</label>
+                                <textarea id="comments" class="form-control" name="comments" rows="3" type="text" wrap="soft"></textarea>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col">
+<!--                            <button type="submit" class="btn btn-primary mb-1" name="submit">Submit</button>-->
+                            <input id="cmdSubmit" class="btn btn-sm btn-success btn-block" type="submit" value="Get Started">
+                        </div>
+                    </div>
+
+                    <input type="hidden" id="RestaurantMarketingAgent" name="agent" value="Other">
+                    <input type="hidden" id="SignupFormVersion" name="version" value="L4U Website 1.0" />
+
+                </div>
+            </div>
+
+            <input type="hidden" id="formType" name="formType" value="mini" />
+            <input type="hidden" id="leadSource" name="leadSource" value="Landing Page" />
+            <input type="hidden" id="leadRecordType" name="leadRecordType" value="Ads" />
+        </form>
+    </div>
+
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://report.localforyou.com/modules/signupMini/assets/js/index.js?v=2.0.1"></script>
+<script>
+    const miniForm = $("#miniForm");
+
+    miniForm.submit(function (e) {
+        e.preventDefault();
+        const isValid = validateForm(miniForm);
+        if (!isValid) return false;
+        setMoney();
+        const payload = getPayload(miniForm);
+        sendPayload(payload);
+    });
+
+</script>
+</body>
+
+</html>
--- modules/signupMini/assets/js/--index.js
+++ modules/signupMini/assets/js/--index.js
@@ -1,3 +1,4 @@
+
 function setMoney() {
     let country = $("#country").val();
     let currencyBox = $("#currency");
@@ -10,12 +11,10 @@ function setMoney() {
         currencyBox.val("NZD");
     }else if(country === "United Kingdom"){
         currencyBox.val("GBP");
-    }else if(country === "United States"){
+    }else if(country === "USA"){
         currencyBox.val("USD");
     }else if(country === "Thailand"){
         currencyBox.val("THB");
-    }else{
-        currencyBox.val("");
     }
 }//setMoney
 
@@ -33,18 +32,16 @@ function shortCountry() {
         return "US";
     }else if(country === "Thailand"){
         return "TH";
-    }else{
-        return "";
     }
 }
 
 function shopTypeForLeadManagement() {
     let shopType = $("#shopType").val();
-    if (shopType === "Thai Restaurants &amp; Takeaways"){
+    if (shopType === "Thai Restaurants & Takeaways"){
         return "Thai Restaurant";
     }else if (shopType === "Thai Massage"){
         return "Thai Massage";
-    }else if (shopType === "Restaurants &amp; Takeaways"){
+    }else if (shopType === "Restaurants & Takeaways"){
         return "Restaurant";
     }
 }
@@ -74,65 +71,49 @@ function getPayload(form) {
     };
 }
 
+function sendPayload(payload) {
+    console.log("🚀 Sending Payload:", payload);
+    $.ajax({
+        url: "https://hook.us1.make.com/47ue45ij7fhm7sol8rldp6dxpag2ldjl",
+        method: "POST",
+        dataType: "json",
+        data: payload,
+        success: function (response) {
+            console.log("✅ Webhook Success:", response);
+            if (response.result === "Leads to Monday successfully") {
+                $("#successMessage").show();
+                setTimeout(() => {
+                    window.location.href = "https://localforyou.com/thank-you/";
+                }, 1500);
+            }
+        },
+        error: function (xhr, status, error) {
+            console.error("❌ Webhook Failed:", status, error);
+        }
+    });
+}
+
 function validateForm(form) {
     let isValid = true;
-    form.find(".field-error").remove(); // Clear previous errors
-    form.find(".border-red-400").removeClass("border-red-400");
+    form.find(".text-danger").remove(); // Clear previous errors
 
     const firstName = form.find('[name="first_name"]');
     const email     = form.find('[name="email"]');
     const mobile    = form.find('[name="mobile"]');
-    const lastName  = form.find('[name="last_name"]');
-    const shopName  = form.find('[name="shopName"]');
-    const shopType  = form.find('[name="shopType"]');
+    const shopType   = form.find('[name="shopType"]');
     const country   = form.find('[name="country"]');
-    const currency  = form.find('[name="currency"]');
-    const interest  = form.find('[name="interest"]');
 
     function showError(input, message) {
-        const error = $('<p class="field-error text-red-500 text-xs mt-1"></p>').text(message);
-        input.addClass("border-red-400");
-        input.closest('.form-group').append(error);
+        const error = $('<small class="text-danger d-block mt-1"></small>').text(message);
+        input.after(error);
         isValid = false;
     }
 
-    if (!firstName.val() || !firstName.val().trim()) showError(firstName, "First name is required.");
-    if (!lastName.val() || !lastName.val().trim()) showError(lastName, "Last name is required.");
-    if (!email.val() || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.val())) showError(email, "Invalid email.");
-    if (!mobile.val() || !/^\+?[0-9\s\-]{8,15}$/.test(mobile.val())) showError(mobile, "Invalid mobile number.");
-    if (!shopName.val() || !shopName.val().trim()) showError(shopName, "Shop name is required.");
-    if (!shopType.val() || shopType.val() === "") showError(shopType, "Please select a business type.");
-    if (!country.val() || country.val() === "") showError(country, "Please select a country.");
-    if (!currency.val() || currency.val() === "") showError(currency, "Please select a currency.");
-    if (!interest.val() || interest.val() === "") showError(interest, "Please select your interest.");
+    if (!firstName.val().trim()) showError(firstName, "First name is required.");
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.val())) showError(email, "Invalid email.");
+    if (!/^\+?[0-9\s\-]{8,15}$/.test(mobile.val())) showError(mobile, "Invalid mobile number.");
+    if (!shopType.val()) showError(shopType, "Please select a business type.");
+    if (!country.val()) showError(country, "Please select a country.");
 
     return isValid;
 }
-
-function getUTMParams() {
-    const params = new URLSearchParams(window.location.search);
-    return {
-        utm_source: params.get("utm_source") || "",
-        utm_medium: params.get("utm_medium") || "",
-        utm_campaign: params.get("utm_campaign") || "",
-        utm_content: params.get("utm_content") || "",
-        utm_term: params.get("utm_term") || ""
-    };
-}
-
-sendPayload = function(payload) {
-    console.log("🚀 Sending Payload with UTM:", payload);
-
-    try {
-        fbq("track", "Lead", {
-            content_name: "Thai Demo Signup Form",
-            currency: payload.currency || "THB",
-            value: 0,
-            utm_source: payload.utm_source,
-            utm_medium: payload.utm_medium,
-            utm_campaign: payload.utm_campaign
-        });
-    } catch (e) {
-        console.warn("⚠️ FB Pixel not loaded yet:", e);
-    }
-};
