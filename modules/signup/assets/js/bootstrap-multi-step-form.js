27419b50aee3e91ed7391ef9fef7b8073663ab6c
BasBYY <bas@localforyou.com>
2026-04-08 15:29:28 +0700

    update coupon

--- modules/signup/assets/js/bootstrap-multi-step-form.js
+++ modules/signup/assets/js/bootstrap-multi-step-form.js
@@ -1363,7 +1363,7 @@ function applyCoupon() {
     $("#couponCode2").attr('disabled', 'disabled');
   }
 
-  const availableCoupon = new Set(["1TRIAL", "1SMILE", "WAWIO", "FREEWEB", "PARTNER96", "PARTNER98", "PARTNER195", "PARTNER198", "PARTNER246", "PARTNER268", "PARTNER118","ONCE108","ONCE158","ONCE198","ONCE246","ONCE268","ONCE348","AIUS"]);
+  const availableCoupon = new Set(["1TRIAL", "1SMILE", "WAWIO", "FREEWEB", "PARTNER96", "PARTNER98", "PARTNER195", "PARTNER198", "PARTNER246", "PARTNER268", "PARTNER118","PARTNER298","PARTNER368","ONCE108","ONCE158","ONCE198","ONCE246","ONCE268","ONCE348","AIUS","ARAYAAI"]);
 
 
   inputCode = inputCode.toUpperCase();
--- modules/signup/form_footer.php
+++ modules/signup/form_footer.php
@@ -27,13 +27,13 @@
 <script src="assets/js/jquery.3.6.0.min.js"></script>
 <script src="assets/js/bootstrap5.0.2.bundle.min.js"></script>
 <script src="assets/js/global_data.js?v=2.0.10"></script>
-<script src="assets/js/settings.js?v=2.0.25"></script>
+<script src="assets/js/settings.js?v=2.0.27"></script>
 <script src="assets/js/date_format.js"></script>
 <script src="assets/js/getUserAgent.js"></script>
 <script src="assets/js/util.js?v=2.0.10"></script>
 <script src="assets/js/ajaxFunction.js?v=2.0.35"></script>
 <script src="https://api.ipify.org?format=jsonp&callback=getIP"></script>
 <script src="assets/js/popper.2.11.5.min.js"></script>
-<script src="assets/js/bootstrap-multi-step-form.js?v=2.0.28"></script>
+<script src="assets/js/bootstrap-multi-step-form.js?v=2.0.31"></script>
 
-<input type="hidden" id="signupFormVersion" name="signupFormVersion" value="2.10.33" />
+<input type="hidden" id="signupFormVersion" name="signupFormVersion" value="2.10.35" />
