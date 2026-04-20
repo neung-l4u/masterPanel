e43adedd394b8d9d58ca4cd9690e6886ac30bace
markL4U <mark@localforyou.com>
2026-04-20 09:04:47 +0700

    done

---
+++ modules/aiAraya/models/customerDetailsForm.php
@@ -0,0 +1,21 @@
+<?php
+global $db;
+session_start();
+header('Content-Type: application/json');
+include '../assets/db/db.php';
+include '../assets/db/initDB.php';
+
+$result["result"] = "";
+$result["msg"] = "";
+
+$customerDetailsLogs = !empty($_POST["payload"]) ? $_POST["payload"] : "no data";
+$stripeID = !empty($_POST["stripeID"]) ? $_POST["stripeID"] : "";
+$logsToDB =  $db->query('INSERT INTO `ai_araya`(`customerDetailsLogs`, `stripeID`) VALUES (?, ?)', [$customerDetailsLogs, $stripeID]);
+
+$updateStatus = $db->query('UPDATE ai_araya SET statusUser = "completed" , reminderStep = 1  WHERE stripeID = ?', [$stripeID]);
+
+$result["result"] = "success";
+$result["msg"] = "Save to DB successfully!";
+
+echo json_encode($result);
+?>
