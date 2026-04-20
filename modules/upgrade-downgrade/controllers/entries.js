06f14d2d5eed8c6e3b9ddceba85023bb6d651798
markL4U <mark@localforyou.com>
2026-04-20 10:16:20 +0700

    re upgrade downgrade form

--- modules/upgrade-downgrade/controllers/entries.js
+++
@@ -1,8 +0,0 @@
-function initTooltips() {
-    const tooltipList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-    tooltipList.map(function (el) {
-        return new bootstrap.Tooltip(el, {
-            customClass: 'my-tooltip'
-        });
-    });
-}
